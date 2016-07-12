14070b3d6f25b50e13302dbebbc70386b79da774
Raphael Barbo <rsbarbo@gmail.com>
2016-07-12 11:07:57 -0600

    needs to fix some aspects of the file_decoder

---
+++ test/file_decoder_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/file_decoder"
+
+class FileDecoderTest < Minitest::Test
+
+  def test_read_file
+    file_enc = ParaMorse::FileDecoder.new
+    assert_equal 10, file_enc.decode("test/test_2.txt", "test/test_1.txt")
+  end
+
+
+end
