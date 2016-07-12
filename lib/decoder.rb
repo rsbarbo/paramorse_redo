7dbe30515d5d8eb32913bb94bf1bd56583a37849
Raphael Barbo <rsbarbo@gmail.com>
2016-07-12 10:15:06 -0600

    latest update

--- lib/decoder.rb
+++ lib/decoder.rb
@@ -26,29 +26,3 @@ module ParaMorse
     end
 
   end
-
-  #   binding.pry
-  #   decoded_word = ""
-  #   morse_code.each_char.with_index do |digit, index|
-  #     queue.push(digit)
-  #     if queue_ends_with_space?(queue)
-  #       encoded_letter = get_next_letter(queue)
-  #     elsif end_of_morse_code_message?(index, morse_code)
-  #       encoded_letter = get_last_letter(queue)
-  #     end
-  #     if encoded_letter
-  #       decoded_word += letter_decoder.decode(encoded_letter)
-  #       queue.clear
-  #     end
-  #   end
-  #   return decoded_word
-  # end
-
-
-  # def get_last_letter(queue)
-  #   queue.peek(queue.count).join
-  # end
-
-  # def get_next_letter(queue)
-  #   encoded_letter = queue.peek(queue.count - 3).join
-  # end
