feabb6d3da196f214418ced5e3521f481c51c67b
Raphael Barbo <rsbarbo@gmail.com>
2016-07-13 15:23:08 -0600

    just made the file encoder more dinamic so I dont have to perform the same operation

--- lib/file_encoder.rb
+++ lib/file_encoder.rb
@@ -8,14 +8,25 @@ module ParaMorse
       @encoder = ParaMorse::Encoder.new
     end
 
-    def encode(plain_text, encoded_text)
+    def encode(plain_text, nof = 0, encoded_files)
       input = File.read(plain_text)
       output = encoder.encode(input)
-      File.write(encoded_text, output)
+      file_counter = (0..(nof -1)).to_a
+      if encoded_files.include?("*")
+        file_counter.each_with_index do |number, index|
+          if index < 10
+            File.write("output0#{n}.txt", output)
+          else
+            File.write("output#{n}.txt", output)
+          end
+        end
+      else
+        File.write(encoded_files, output)
+      end
     end
-  end
 
+  end
 end
 
-file_enc = ParaMorse::FileEncoder.new
-file_enc.encode("plain.txt", "encoded.txt")
+  file_enc = ParaMorse::FileEncoder.new
+  file_enc.encode("plain.txt", "encoded.txt")
--- lib/para_encoder.rb
+++ lib/para_encoder.rb
@@ -5,30 +5,3 @@ module ParaMorse
   class ParallelEncoder
 
     attr_reader :encoder
-
-    def initialize
-      @encoder = ParaMorse::Encoder.new
-    end
-
-    def encode(plain_text, nof, encoded_files)
-      input = File.read(plain_text)
-      output = encoder.encode(input)
-      file_array = (0..(nof -1)).to_a
-      if encoded_files.include?("*")
-        file_array.each_with_index do |n, i|
-          if i < 10
-            File.write("output0#{n}.txt", output)
-          else
-            File.write("output#{n}.txt", output)
-          end
-        end
-      else
-        encoded_files
-      end
-    end
-
-  end
-end
-
-  file_enc = ParaMorse::ParallelEncoder.new
-  file_enc.encode("plain.txt", 11, "output*.txt")
