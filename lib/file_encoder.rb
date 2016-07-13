787240b1b6315164a78b6611e7286a1887ebd9c4
Raphael Barbo <rsbarbo@gmail.com>
2016-07-13 15:40:15 -0600

    file_encoder is more dinamic now, and methods are broken down into smaller pieces

--- lib/file_encoder.rb
+++ lib/file_encoder.rb
@@ -11,22 +11,30 @@ module ParaMorse
     def encode(plain_text, nof = 0, encoded_files)
       input = File.read(plain_text)
       output = encoder.encode(input)
+      sort_writer(nof, encoded_files, output)
+    end
+
+    def sort_writer(nof, encoded_files, output)
       file_counter = (0..(nof -1)).to_a
       if encoded_files.include?("*")
-        file_counter.each_with_index do |number, index|
-          if index < 10
-            File.write("output0#{n}.txt", output)
-          else
-            File.write("output#{n}.txt", output)
-          end
-        end
+        file_breaker(file_counter, nof, output)
       else
         File.write(encoded_files, output)
+      end
+    end
+
+    def file_breaker(file_counter, nof, output)
+      file_counter.each_with_index do |number, index|
+        if index < 10
+          File.write("output0#{number}.txt", output)
+        else
+          File.write("output#{number}.txt", output)
+        end
       end
     end
 
   end
 end
 
-  file_enc = ParaMorse::FileEncoder.new
-  file_enc.encode("plain.txt", "encoded.txt")
+file_enc = ParaMorse::FileEncoder.new
+file_enc.encode("plain.txt", 2, "output*.txt")
