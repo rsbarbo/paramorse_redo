728865bcf645d85ba136eb94b521e911811149ca
Raphael Barbo <rsbarbo@gmail.com>
2016-07-13 20:15:58 -0600

    adjust lines:

--- test/para_encoder_test.rb
+++ test/para_encoder_test.rb
@@ -3,11 +3,9 @@ require "./lib/para_encoder"
 
 class ParallelEncoderTest < Minitest::Test
 
-def test_something
-  parallel_encoder = ParaMorse::ParallelEncoder.new
-  assert_equal [0, 1, 2, 3, 4, 5, 6, 7], parallel_encoder.encode_from_file('input.txt', 8, 'output*.txt')
-end
-
-
+  def test_something
+    parallel_encoder = ParaMorse::ParallelEncoder.new
+    assert_equal [0, 1, 2, 3, 4, 5, 6, 7], parallel_encoder.encode_from_file('input.txt', 8, 'output*.txt')
+  end
 
 end
